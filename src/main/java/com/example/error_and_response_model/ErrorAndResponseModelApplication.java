31d4f58add9405215a9652e2283d505898b7a86e
meanzi3 <minjichoe0326@gmail.com>
2024-04-10 23:37:04 +0900

    init commit

---
+++ src/main/java/com/example/error_and_response_model/ErrorAndResponseModelApplication.java
@@ -0,0 +1,13 @@
+package com.example.error_and_response_model;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErrorAndResponseModelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErrorAndResponseModelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/error_and_response_model/ErrorAndResponseModelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.error_and_response_model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErrorAndResponseModelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
