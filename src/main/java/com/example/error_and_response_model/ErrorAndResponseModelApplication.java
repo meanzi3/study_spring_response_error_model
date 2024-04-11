b4c8b0bf67fd7189bda3a8d540def48bcaa74b7c
meanzi3 <minjichoe0326@gmail.com>
2024-04-11 12:32:58 +0900

    fix: DB 읽기 오류 수정
    - 해당 프로젝트에 DB관련 정보가 정의되어 있지 않기 때문에 DB를 읽어올 수 없다며 에러가 발생함.
    - 이번 경우, DB가 필요하지 않은 간단한 상황으로 `@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)` 어노테이션을 추가하여서 해결함.

--- src/main/java/com/example/error_and_response_model/ErrorAndResponseModelApplication.java
+++ src/main/java/com/example/error_and_response_model/ErrorAndResponseModelApplication.java
@@ -2,8 +2,9 @@ package com.example.error_and_response_model;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class ErrorAndResponseModelApplication {
 
 	public static void main(String[] args) {
